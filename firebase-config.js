675e93d0f912393576a5b2961d50476e6087ee21
alonsozorteodos-bit <alonsozorteodos@gmail.com>
2026-07-23 13:33:29 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,21 @@
+// Importa las funciones necesarias de Firebase v10
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
+
+// TU CONFIGURACIÓN DE FIREBASE (reemplaza con tus datos reales)
 const firebaseConfig = {
-  apiKey: "AIzaSyCfac-HDMwrLyOURWhQQ3cMnugkRB13UFo",
-  authDomain: "tienda-peru-constanza.firebaseapp.com",
-  projectId: "tienda-peru-constanza",
-  storageBucket: "tienda-peru-constanza.firebasestorage.app",
-  messagingSenderId: "613245426769",
-  appId: "1:613245426769:web:99df3dd599f240af2aac70",
-  measurementId: "G-09XTFW0ETT"
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TUS_DATOS",
+  appId: "TUS_DATOS"
 };
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
